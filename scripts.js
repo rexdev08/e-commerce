43c9cf2debcc887bed253c4a7947e9a18f69b4ed
rexdev08 <raimundorincong@gmail.com>
2022-10-27 01:20:12 -0300

    "cambie imagen principal y colores"

--- scripts.js
+++ scripts.js
@@ -7,16 +7,13 @@ let shoppingCart = [];
 const getId = (element) => document.getElementById(element);
 const $btnShoppingCart = getId("btn-shopping-cart");
 const $modal = getId("modal");
-const $productsContainer = getId("products-container")
-const $btnMenu = getId("btn-menu")
-const $headerMenu = getId("header-menu")
-const $sofas = getId("sofas")
-const $veladores = getId("veladores")
-const $btnClose = getId("btn-close")
-const $modalItems = getId("modal-items")
-
-
-
+const $productsContainer = getId("products-container");
+const $btnMenu = getId("btn-menu");
+const $headerMenu = getId("header-menu");
+const $sofas = getId("sofas");
+const $veladores = getId("veladores");
+const $btnClose = getId("btn-close");
+const $modalItems = getId("modal-items");
 const showModal = () => $modal.classList.toggle("modal--active");
 const showMenu = () => $headerMenu.classList.toggle("header__menu--active");
 
@@ -25,10 +22,11 @@ function renderProduct(obj) {
             <img loading="lazy" src="${obj.image}" alt="" class="item__image">
             <h3 class="item__h3">${obj.product}</h3>
             <span class="item__span">$${obj.price.toLocaleString()}</span>
-            <button data-key="${obj.id}" id="product-${obj.id}" class="item__button">anadir al carrito</button>
-          </div>`
+            <button data-key="${obj.id}" id="product-${
+    obj.id
+  }" class="item__button">anadir al carrito</button>
+          </div>`;
 }
-
 
 const renderItem = (obj) => {
   return ` 
@@ -45,7 +43,9 @@ const renderItem = (obj) => {
         <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-dash-lg" viewBox="0 0 16 16">
           <path fill-rule="evenodd" d="M2 8a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11A.5.5 0 0 1 2 8Z" />
         </svg></button>
-      <span data-identifier="${obj.id}" id="quantity" class="item__quantity">${obj.quantity}</span>
+      <span data-identifier="${obj.id}" id="quantity" class="item__quantity">${
+    obj.quantity
+  }</span>
       <button id="btn-right" class="item__button item__button--right"><svg xmlns="http://www.w3.org/2000/svg"
           fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
           <path
@@ -57,116 +57,118 @@ const renderItem = (obj) => {
 };
 
 function updateTotal() {
-  const $total = document.getElementById('total')
-  const total = shoppingCart.map((obj) => {
-    return obj.price * obj.quantity
-  }).reduce((value, nextValue) => {
-    return value + nextValue
-  }, 0).toLocaleString()
-  $total.innerText = total
-  const $headerCounter = document.getElementById("header-counter")
+  const $total = document.getElementById("total");
+  const total = shoppingCart
+    .map((obj) => {
+      return obj.price * obj.quantity;
+    })
+    .reduce((value, nextValue) => {
+      return value + nextValue;
+    }, 0)
+    .toLocaleString();
+  $total.innerText = total;
+  const $headerCounter = document.getElementById("header-counter");
 
-const itemsTotal = shoppingCart.map((obj) => {
-  return  obj.quantity
-}).reduce((value, nextValue) => {
-  return value + nextValue
-}, 0)
+  const itemsTotal = shoppingCart
+    .map((obj) => {
+      return obj.quantity;
+    })
+    .reduce((value, nextValue) => {
+      return value + nextValue;
+    }, 0);
 
-  $headerCounter.innerText = itemsTotal
+  $headerCounter.innerText = itemsTotal;
 
-  $headerCounter.innerText > 0 ? $headerCounter.classList.add("header__counter--active"):$headerCounter.classList.remove("header__counter--active")
-};
-
+  $headerCounter.innerText > 0
+    ? $headerCounter.classList.add("header__counter--active")
+    : $headerCounter.classList.remove("header__counter--active");
+}
 
 function updateCar() {
-  $modalItems.innerHTML = ""
-  shoppingCart.forEach(product => {
-
-    const div = document.createElement("div")
-    div.className = "item"
-    div.dataset.id = product.id
-    div.innerHTML = renderItem(product)
-    $modalItems.append(div)
-  })
+  $modalItems.innerHTML = "";
+  shoppingCart.forEach((product) => {
+    const div = document.createElement("div");
+    div.className = "item";
+    div.dataset.id = product.id;
+    div.innerHTML = renderItem(product);
+    $modalItems.append(div);
+  });
 }
 
 function filterProducts(element = "sofa") {
-  const selection = storage.filter(obj => obj.category === element);
+  const selection = storage.filter((obj) => obj.category === element);
 
-  $productsContainer.innerHTML = selection.map(product => renderProduct(product)).join("")
+  $productsContainer.innerHTML = selection
+    .map((product) => renderProduct(product))
+    .join("");
 
-
-  const $btnAdd = document.querySelectorAll(".item__button")
-  $btnAdd.forEach(boton => {
+  const $btnAdd = document.querySelectorAll(".item__button");
+  $btnAdd.forEach((boton) => {
     boton.addEventListener("click", () => {
-      const product = storage.find(obj => obj.id == boton.dataset.key)
+      const product = storage.find((obj) => obj.id == boton.dataset.key);
       if (!shoppingCart.includes(product)) {
-        shoppingCart.push(product)
+        shoppingCart.push(product);
 
-        updateTotal()
+        updateTotal();
       }
 
+      updateCar();
 
-      updateCar()
-
-      const btnsRemove = $modalItems.querySelectorAll(".item__btn-delete")
-      btnsRemove.forEach(btn => {
+      const btnsRemove = $modalItems.querySelectorAll(".item__btn-delete");
+      btnsRemove.forEach((btn) => {
         btn.addEventListener("click", () => {
-          btn.parentElement.parentElement.remove()
-          shoppingCart = shoppingCart.filter(obj => obj.id != btn.parentElement.parentElement.dataset.id)
-          storage.find(obj => obj.id == btn.parentElement.parentElement.dataset.id).quantity = 1
+          btn.parentElement.parentElement.remove();
+          shoppingCart = shoppingCart.filter(
+            (obj) => obj.id != btn.parentElement.parentElement.dataset.id
+          );
+          storage.find(
+            (obj) => obj.id == btn.parentElement.parentElement.dataset.id
+          ).quantity = 1;
 
+          updateTotal();
+        });
+      });
 
-
-          updateTotal()
-        })
-
-      })
-
-
-
-
-
-      const btnMinus = $modalItems.querySelectorAll("#btn-left")
-      btnMinus.forEach(btn => {
+      const btnMinus = $modalItems.querySelectorAll("#btn-left");
+      btnMinus.forEach((btn) => {
         btn.addEventListener("click", () => {
-          const productId = btn.parentElement.parentElement.parentElement.dataset.id
-          const qty = $modalItems.querySelector(`[data-identifier="${productId}"]`)
-          const product = storage.filter(obj => obj.id === parseInt(productId))
+          const productId =
+            btn.parentElement.parentElement.parentElement.dataset.id;
+          const qty = $modalItems.querySelector(
+            `[data-identifier="${productId}"]`
+          );
+          const product = storage.filter(
+            (obj) => obj.id === parseInt(productId)
+          );
           if (product[0].quantity > 1) {
-
-            product[0].quantity--
+            product[0].quantity--;
           }
-          qty.textContent = product[0].quantity
+          qty.textContent = product[0].quantity;
 
-          updateTotal()
-        })
-      })
+          updateTotal();
+        });
+      });
 
-      const btnPlus = $modalItems.querySelectorAll("#btn-right")
-      btnPlus.forEach(btn => {
+      const btnPlus = $modalItems.querySelectorAll("#btn-right");
+      btnPlus.forEach((btn) => {
         btn.addEventListener("click", () => {
-          const productId = btn.parentElement.parentElement.parentElement.dataset.id
-          const qty = $modalItems.querySelector(`[data-identifier="${productId}"]`)
-          const product = storage.filter(obj => obj.id === parseInt(productId))
-          product[0].quantity++
-          qty.textContent = product[0].quantity
+          const productId =
+            btn.parentElement.parentElement.parentElement.dataset.id;
+          const qty = $modalItems.querySelector(
+            `[data-identifier="${productId}"]`
+          );
+          const product = storage.filter(
+            (obj) => obj.id === parseInt(productId)
+          );
+          product[0].quantity++;
+          qty.textContent = product[0].quantity;
 
-
-          updateTotal()
-        })
-      })
-
-
-    })
-  })
-
-
-
-
-
+          updateTotal();
+        });
+      });
+    });
+  });
 }
-
 
 // logica
 
@@ -176,11 +178,9 @@ $btnMenu.addEventListener("click", showMenu);
 $sofas.addEventListener("click", () => {
   filterProducts("sofa");
   $headerMenu.classList.remove("header__menu--active");
-})
+});
 $veladores.addEventListener("click", () => {
   filterProducts("velador");
   $headerMenu.classList.remove("header__menu--active");
-})
-filterProducts()
-
-
+});
+filterProducts();
